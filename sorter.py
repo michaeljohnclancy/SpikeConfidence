5bd1e395379ed66f9f3ced59e2567eaeb1da5e30
Michael Clancy <mjclancy@protonmail.ch>
2020-12-10 21:44:54 +0000

    Example of regression to predict hungarian agreement score

--- sorter.py
+++ sorter.py
@@ -1,24 +1,25 @@
+import pandas as pd
 import json
 import shutil
 from pathlib import Path
 from typing import List, Dict
 import spikeextractors as se
+import spiketoolkit as st
+from spikecomparison import GroundTruthComparison
 from spikecomparison.studytools import load_probe_file_inplace
 from spikesorters import run_sorter
 from spikeextractors import MEArecRecordingExtractor, NpzSortingExtractor, MEArecSortingExtractor, SortingExtractor
 
 
 class SpikeSorter:
-    def __init__(self, dataset_path: Path,
-                 sorter_names: List[str]):
+    def __init__(self, dataset_path: Path):
 
         self.dataset_path = dataset_path
-        self.sorter_names = sorter_names
         self.cache_path = dataset_path.parent / '.cache'
         self.recording_cache = self.cache_path / 'recording'
         self.ground_truth_cache = self.cache_path / 'ground_truth.npz'
         self.sorting_cache = self.cache_path / 'sortings'
-        self.metrics_cache = self.cache_path / 'metrics.feather'
+        self.metrics_cache = self.cache_path / 'metrics'
         self.raw_data_path = self.recording_cache / 'raw.dat'
         self.prb_path = self.recording_cache / 'probes.prb'
         self.info_path = self.recording_cache / 'info.json'
@@ -57,37 +58,65 @@ class SpikeSorter:
             NpzSortingExtractor.write_sorting(ground_truth, save_path=str(self.ground_truth_cache))
             return NpzSortingExtractor(self.ground_truth_cache)
 
-    def get_sortings(self) -> Dict[str, NpzSortingExtractor]:
-        return {sorter_name: self.get_sorting(sorter_name) for sorter_name in self.sorter_names}
+    def get_sortings(self, sorter_names: List[str]) -> Dict[str, NpzSortingExtractor]:
+        return {sorter_name: self.get_sorting(sorter_name) for sorter_name in sorter_names}
 
     def get_sorting(self, sorter_name) -> NpzSortingExtractor:
-        s_cache = self.sorting_cache / f'{sorter_name}.npz'
-        if s_cache.exists():
+        s_cache = self.sorting_cache / sorter_name
+        s_npz_cache = s_cache.with_suffix('.npz')
+        if s_npz_cache.exists():
             try:
-                return NpzSortingExtractor(s_cache)
+                return NpzSortingExtractor(s_npz_cache)
             except:
                 print(f"Error reading {sorter_name} cache, deleting...")
-                shutil.rmtree(s_cache)
+                s_npz_cache.unlink()
                 return self.get_sorting(sorter_name)
 
         else:
             print(f"Running {sorter_name}...")
-            sorting: SortingExtractor = run_sorter(
+            s_cache.mkdir()
+            sorting = run_sorter(
                 sorter_name,
                 recording=self.recording,
                 output_folder=s_cache,
-                delete_output_folder=True
             )
 
-            NpzSortingExtractor.write_sorting(sorting, save_path=str(s_cache))
+            NpzSortingExtractor.write_sorting(sorting, save_path=str(s_npz_cache))
+
+            shutil.rmtree(s_cache)
+
             return NpzSortingExtractor(s_cache)
 
-    # def get_sorter_metrics(self, sorter_name: str, metrics_names: List[str]):
-    #     m_cache = self.sorting_cache / sorter_name / 'metrics'
-    #     try:
-    #         m_cache.mkdir(parents=True, exist_ok=False)
-    #     except FileExistsError:
-    #         sorter_class = sorter_dict[sorter_name]
+    def get_sorter_metrics(self, sorter_name: str, metric_names: List[str]):
+        self.metrics_cache.mkdir(exist_ok=True)
+        metrics_cache = self.metrics_cache / f'{sorter_name}.feather'
+        sorting = self.get_sorting(sorter_name)
+        if metrics_cache.exists():
+            metrics = pd.read_parquet(metrics_cache)
+            metrics_to_calc = [m for m in metric_names if m not in metrics.columns]
+
+            if metrics_to_calc:
+                new_metrics = st.validation.compute_quality_metrics(
+                    sorting, self.recording,
+                    metric_names=metrics_to_calc,
+                    as_dataframe=True
+                )
+                metrics = pd.concat([metrics, new_metrics], axis=1)
+                metrics_cache.unlink()
+                metrics.to_parquet(str(metrics_cache))
+
+            return metrics[metric_names]
+        else:
+            metrics = st.validation.compute_quality_metrics(
+                sorting, self.recording,
+                metric_names=metric_names,
+                as_dataframe=True)
+            metrics.to_parquet(str(metrics_cache))
+            return metrics
+
+    def get_ground_truth_comparison(self, sorter_name: str) -> GroundTruthComparison:
+        sorting = self.get_sorting(sorter_name)
+        return GroundTruthComparison(self.ground_truth, sorting)
 
     def _cache_recording(self, recording_extractor):
         """ If the recording exists in the cache, just return it.
---
+++ utils.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+from sorter import SpikeSorter
+
+
+def prepare_hungarian_agreement_dataset(dataset_path: Path, sorter_names: List[str],
+                    metric_names: List[str], shuffle: bool = True,
+                    drop_nans: bool = True):
+
+    session = SpikeSorter(dataset_path)
+
+    X = np.empty(shape=(0, len(metric_names)))
+    y = np.array([])
+    for sorter_name in sorter_names:
+        metrics = session.get_sorter_metrics(sorter_name=sorter_name, metric_names=metric_names)
+        ground_truth_comparison = session.get_ground_truth_comparison(sorter_name=sorter_name)
+
+        agreement_scores = ground_truth_comparison.agreement_scores.iloc[ground_truth_comparison.best_match_21, :]
+        hungarian_agreement_scores = pd.Series(np.diag(agreement_scores))
+
+        X = np.vstack([X, metrics])
+        y = np.append(y, hungarian_agreement_scores)
+
+    if drop_nans:
+        nan_index = ~np.isnan(X).any(axis=1)
+        X = X[nan_index]
+        y = y[nan_index]
+
+    if shuffle:
+        index = np.arange(X.shape[0])
+        np.random.shuffle(index)
+        X = X[index].squeeze()
+        y = y[index].squeeze()
+
+    return X, y
